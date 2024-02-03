9d73663921f785d68bcc4782f3b7a4ec7a290107
Zack Ames <zames6584@gmail.com>
2024-02-02 19:26:07 -0500

    get working

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const exphbs = require('express-handlebars');
 const Handlebars = require("handlebars");
+const helpers = require("./lib/helpers");
 
 const { createCanvas } = require('canvas');
-const { templateHTML, frameHTML } = require("./template");
-
 
 const {
     FrameRequest,
@@ -12,32 +11,20 @@ const {
     getFrameHtmlResponse,
 } =require('@coinbase/onchainkit');
 
-  
 const app = express();
-app.engine('handlebars', exphbs.engine({ 
-    helpers: {
-      canvasUrl: function (fid) {
-        // Create a canvas and get its 2D context
-        const canvas = createCanvas(width, height);
-        
-        canvas.width=640;canvas.height=360;
-        var ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'red';
-        ctx.fillRect(0,80,128,16);
-        ctx.fillRect(16,0,32,32);
-        ctx.fillRect(80,0,32,32);
-    
-        ctx.fillStyle = 'black';
-        ctx.fillText(fid, 0, 0);
-    
-        // Convert the canvas to a data URL
-        const dataURL = canvas.toDataURL();
-    
-        // Return the HTML with the data URL
-        return dataURL;
-    }
-    } 
-  }));
+
+var hbs = exphbs.create({
+    helpers      : helpers,
+
+    // Uses multiple partials dirs, templates in "shared/templates/" are shared
+    // with the client-side of the app (see below).
+    partialsDir: [
+        'views/'
+    ]
+});
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
@@ -45,17 +32,15 @@ const port = process.env.PORT || 3001;
 const url = "https://handlebars-puppeteer-frame-production.up.railway.app";
 
 
+
 app.get('/', async (req, res) => {
    // let {logoUrl, title, tags, path, bgUrl} = req.query;
     const body = await req.body;
 
-    const compiledHTML = Handlebars.compile(templateHTML)({
-        url
-      });
-
-    console.log(compiledHTML);
-  
-    res.status(200).send(compiledHTML);
+    res.render('page', {
+        url: url
+        }
+    );
 })
 
 app.post('/frame', async (req, res) => {
@@ -63,12 +48,10 @@ app.post('/frame', async (req, res) => {
     console.log(req.body)
     let body = await req.body;
     let fid = body.untrustedData.fid
-    
-    const compiledFrame = Handlebars.compile(frameHTML)({
+
+    res.render('frame', {
         url, fid
     });
-
-    res.status(200).send(compiledFrame);
 });
 
 app.listen(port, () => {
